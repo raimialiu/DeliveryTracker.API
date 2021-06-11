ef48f5e4638c18d351ca97d4669f309207db8f06
Aliu Raimi Olatunde <Olatunde.Raimi@stanbicibtc.com>
2021-06-11 11:12:31 +0100

    Add project files.

---
+++ DeliveryTracker.API/Models/ItemStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryTracker.API.Models
+{
+    public class ItemStatus
+    {
+        public const string DEVELIVERED = "DELIVERED";
+    }
+}
