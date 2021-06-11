c0808278686a0095dd233b0e69d34816339aa11e
Aliu Raimi Olatunde <Olatunde.Raimi@stanbicibtc.com>
2021-06-11 11:19:16 +0100

    created item status and generic response

---
+++ DeliveryTracker.API/Models/DTOs/ApiResponse/GenericResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryTracker.API.Models.DTOs.ApiResponse
+{
+    public class GenericResponse<T>
+    {
+        public string statusCode { get; set; }
+        public string description { get; set; }
+        public T data { get; set; }
+
+        public GenericResponse(){}
+        public GenericResponse(string statusCode, string description, T data = default) {
+            this.statusCode = statusCode;
+            this.description = description;
+            this.data = data;
+        }
+        public static GenericResponse<T> GetResponse(string statusCode, string description, T data=default)
+        {
+            return new GenericResponse<T>(statusCode, description, data);
+        }
+
+        public static GenericResponse<T> Success(T data)
+        {
+            return GetResponse("00", "request successfull", data);
+        }
+        public static GenericResponse<T> Success(string description, T data)
+        {
+            return GetResponse("00", description, data);
+        }
+
+        public static GenericResponse<T> Failed(string description)
+        {
+            return GetResponse("99", description, default);
+        }
+    }
+}
--- DeliveryTracker.API/Models/ItemStatus.cs
+++ DeliveryTracker.API/Models/ItemStatus.cs
@@ -8,5 +8,8 @@ namespace DeliveryTracker.API.Models
     public class ItemStatus
     {
         public const string DEVELIVERED = "DELIVERED";
+        public const string PICKED_UP = "PICKED_UP";
+        public const string IN_TRANSIT = "IN_TRANSIT";
+        public const string WAREHOUSE = "WAREHOUSE";
     }
 }
