b3f11e36df6ebaa67b9229d73c02a212d73d84ce
Aliu Raimi Tunde <raimialiu428@gmail.com>
2021-06-11 19:29:48 +0100

    test successfullt pass

--- DeliveryTracker.API.UnitTest/BaseTestClass.cs
+++ DeliveryTracker.API.UnitTest/BaseTestClass.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using DeliveryTracker.API.Models;
+using DeliveryTracker.API.Services;
+using DeliveryTracker.API.Services.Data;
 using DeliveryTracker.API.Services.Interfaces;
 using NSubstitute;
 
@@ -12,8 +14,8 @@ namespace DeliveryTracker.API.UnitTest
         protected List<Item> sampleData;
         public BaseTestClass()
         {
-            _service = Substitute.For<IDeliveryTrackingService>(new List<Item>());
-            _query = Substitute.For<IDataQuery>(new List<Item>());
+            _service = Substitute.For<DeliveryTrackerService>(new List<Item>(), null);
+            _query = Substitute.For<DataQuery>(new List<Item>());
             sampleData = _service.GetsampleData();
         }
     }
